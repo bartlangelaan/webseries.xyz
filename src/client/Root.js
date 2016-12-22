ce649e057a411ce51ee6163cf22440234ea3ba5f
Bart Langelaan <langelaanbart@gmail.com>
2016-12-22 18:01:50 +0100

    Change to hash-history

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,13 +1,16 @@
 
 import React from 'react';
-import Router from 'react-router/lib/Router';
-import browserHistory from 'react-router/lib/browserHistory';
+import { Router, useRouterHistory } from 'react-router';
 import routes from '../routes';
+import { createHashHistory } from 'history'
+
+// useRouterHistory creates a composable higher-order function
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 // We need a Root component for React Hot Loading.
 function Root() {
   return (
-    <Router history={browserHistory} routes={routes} />
+    <Router history={appHistory} routes={routes} />
   );
 }
 
