cac8debd450f67481d44acce09323710482b4278
Bart Langelaan <langelaanbart@gmail.com>
2016-12-22 18:32:10 +0100

    Remove all styles

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,10 @@ import Route from 'react-router/lib/Route';
 import IndexRoute from 'react-router/lib/IndexRoute';
 import App from '../components/App';
 import Home from '../components/Home';
-import Tools from '../components/Tools';
 
 const routes = (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
-    <Route path="tools" component={Tools} />
   </Route>
 );
 
