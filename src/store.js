ad191e39d6355b55bf0300f10441924b091458e7
Bart Langelaan <langelaanbart@gmail.com>
2016-12-23 04:18:50 +0100

    Change base url to heroku

--- src/store.js
+++ src/store.js
@@ -1,25 +1,28 @@
 import {observable} from 'mobx';
 
+const BASE_URL = 'https://shielded-shore-55917.herokuapp.com'
+// const BASE_URL = 'http://localhost:3000'
+
 class Store {
   @observable shows = [];
   @observable seasons = [];
   @observable episodes = [];
 
   refreshShows() {
-    fetch('http://localhost:3000/shows').then(res => res.json()).then(json => {
+    fetch(`${BASE_URL}/shows`).then(res => res.json()).then(json => {
       this.shows = json;
     });
   }
 
   refreshSeasons({show}) {
-    fetch(`http://localhost:3000/shows/${show}/seasons`).then(res => res.json()).then(json => {
+    fetch(`${BASE_URL}/shows/${show}/seasons`).then(res => res.json()).then(json => {
       this.seasons = this.seasons.filter(season => season.show != show)
       this.seasons.push(...json);
     });
   }
 
   refreshEpisodes({show, season}) {
-    fetch(`http://localhost:3000/shows/${show}/seasons/${season}/episodes`).then(res => res.json()).then(json => {
+    fetch(`${BASE_URL}/shows/${show}/seasons/${season}/episodes`).then(res => res.json()).then(json => {
       this.episodes = this.episodes.filter(episode => episode.show != show || episode.season != season )
       this.episodes.push(...json);
     });
